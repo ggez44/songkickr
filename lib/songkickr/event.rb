b5696631d934cf44e1e6d90632a8fcfc3e4fc9d6
Andrew Chen <ggez44@gmail.com>
2013-05-02 17:48:35 -0700

    use the datetime with timezone info if available

--- lib/songkickr/event.rb
+++ lib/songkickr/event.rb
@@ -57,8 +57,9 @@ module Songkickr
       
       # Takes the start hash and turns in into a DateTime object.
       def start_hash_to_datetime(start_hash)
-        #datetime = DateTime.parse("#{start_hash["date"]} #{start_hash["time"]}")
-        datetime = start_hash['datetime'].to_datetime
+        start_hash['datetime'].present? \
+            ? start_hash['datetime'].to_datetime \
+            : DateTime.parse("#{start_hash["date"]} #{start_hash["time"]}")
       end
       
       # Builds a list of Performance objects.
